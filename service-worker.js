d2dc76b7094a1910e5b0ded5cac515e7b1d32466
chanwookLee83 <cw2@kakao.com>
2026-09-14 17:00:14 +0900

    Update CACHE_NAME to version v32

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wonrandy-dogam-v31;
+const CACHE_NAME = 'wonrandy-dogam-v32;
 const STATIC_ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
