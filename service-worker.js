76069b39983ef22b4ddb43cf620c9cd12a17460b
chanwookLee83 <cw2@kakao.com>
2026-09-14 09:38:05 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wonrandy-dogam-v25';
+const CACHE_NAME = 'wonrandy-dogam-v26';
 const STATIC_ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
